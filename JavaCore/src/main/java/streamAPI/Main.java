6e26ed36be74fb750654a08eec97e06a638c2c7c
plekhanovsa <atagan@rambler.ru>
2018-10-11 11:03:39 +0300

    in progress

--- JavaCore/src/main/java/streamAPI/Main.java
+++ JavaCore/src/main/java/streamAPI/Main.java
@@ -14,19 +14,31 @@ public class Main {
 
         //Инициализация list
         ArrayList<Integer> list = new ArrayList();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             list.add(i);
         }
 
         //ForkJoinPool используется в параллельных стримах
         ForkJoinPool forkJoinPool = new ForkJoinPool();
+        System.out.println("Количество потоков процессора");
         System.out.println(forkJoinPool.getParallelism());
 
+        System.out.println("Параллельный стрим");
         list.parallelStream().forEach(x -> System.out.println(x));
 
+        System.out.println("Фильтр");
+        list.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+
+        System.out.println("Map");
+        list.stream().map(x -> x*10).collect(Collectors.toList()).forEach(System.out::println);
+
+        System.out.println("Sort");
+        list.stream().sorted().forEach(System.out::println);
+
+        System.out.println("Max");
+        System.out.println( list.stream().max((x,y) -> x-y).get() );
 
 
-        list.stream().filter(x -> x % 2 == 0).map(x -> x * 100).collect(Collectors.toList()).forEach(System.out::println);
     }
 
 }
