4487fe97a2aaea394858381ada58971163b4f6ea
A.A.Plekhanov <A.A.Plekhanov@rambler.ru>
2018-10-14 01:59:23 +0300

    in progress

--- Servlet/src/main/java/plekhanov/servlets/AddServlet.java
+++ Servlet/src/main/java/plekhanov/servlets/AddServlet.java
@@ -2,11 +2,13 @@ package plekhanov.servlets;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 
     @Override
--- Servlet/src/main/java/plekhanov/servlets/ListServlet.java
+++ Servlet/src/main/java/plekhanov/servlets/ListServlet.java
@@ -2,11 +2,14 @@ package plekhanov.servlets;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+@WebServlet("/list")
 public class ListServlet extends HttpServlet {
 
     @Override
