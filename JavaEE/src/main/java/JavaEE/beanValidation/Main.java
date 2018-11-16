a98209876a1adf6fc73a9bf532e2811e96da3ba8
plekhanovsa <atagan@rambler.ru>
2018-11-16 16:17:38 +0300

    in progress

---
+++ JavaEE/src/main/java/JavaEE/beanValidation/Main.java
@@ -0,0 +1,10 @@
+package JavaEE.beanValidation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person person = new Person(null);
+        person.setAdress("dd");
+    }
+}
+
