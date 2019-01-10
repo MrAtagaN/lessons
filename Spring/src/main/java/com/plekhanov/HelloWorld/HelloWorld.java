6af08aaa9fbef3225cee4bed1320aeabac8a7089
plekhanov <atagan@rambler.ru>
2019-01-11 01:25:50 +0300

    in progress

--- Spring/src/main/java/com/plekhanov/HelloWorld/HelloWorld.java
+++ Spring/src/main/java/com/plekhanov/HelloWorld/HelloWorld.java
@@ -2,6 +2,9 @@ package com.plekhanov.HelloWorld;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 /**
  * Created by AtagaN on 11.01.2019.
@@ -9,8 +12,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class HelloWorld {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Conf.class);
-        DaVinchiCode daVinchiCode = applicationContext.getBean(DaVinchiCode.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(Conf.class);
+        //ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:config.xml");
+        //ApplicationContext context = new FileSystemXmlApplicationContext("Spring\\src\\main\\resources\\config.xml");
+
+
+
+        DaVinchiCode daVinchiCode = context.getBean(DaVinchiCode.class);
         daVinchiCode.read();
     }
 }
