b0d1f043c6ac8726c4f4cd00d5090056b1092319
plekhanov <atagan@rambler.ru>
2019-01-21 23:49:11 +0300

    in progress

--- Spring/src/main/java/com/plekhanov/AOP/MyTransactionAspect.java
+++ Spring/src/main/java/com/plekhanov/AOP/MyTransactionAspect.java
@@ -10,22 +10,29 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class MyTransactionAspect {
 
-    @Before("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
+    @Pointcut("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
+    public void myPointcut() {}
+
+    @Before("myPointcut()")
+    //@Before("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
     public void beginTransaction() {
         System.out.println("begin Transaction");
     }
 
-    @After("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
+    @After("myPointcut()")
+    //@After("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
     public void after() {
         System.out.println("after Transaction");
     }
 
-    @AfterReturning("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
+    @AfterReturning("myPointcut()")
+    //@AfterReturning("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
     public void commitTransaction() {
         System.out.println("commit Transaction");
     }
 
-    @AfterThrowing("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
+    @AfterThrowing("myPointcut()")
+    //@AfterThrowing("execution(* com.plekhanov.AOP.MyRepository.getInfo())")
     public void rollback() {
         System.out.println("rollback Transaction");
     }
