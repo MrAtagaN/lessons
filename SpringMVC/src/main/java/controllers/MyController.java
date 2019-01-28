eb9b262c4a314a0bc0229de3b36414de27769274
plekhanov <atagan@rambler.ru>
2019-01-28 23:43:21 +0300

    in progress

--- SpringMVC/src/main/java/controllers/MyController.java
+++ SpringMVC/src/main/java/controllers/MyController.java
@@ -2,8 +2,7 @@ package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by AtagaN on 28.01.2019.
@@ -12,8 +11,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MyController {
 
-    @RequestMapping(value = "/hello", method= RequestMethod.GET)
-    public String hello(Model model) {
-        return "hello";
+    @RequestMapping(value = "/myForm", method= RequestMethod.GET)
+    public String myForm(Model model) {
+        return "myForm";
+    }
+
+    @PostMapping(value = "/myForm")
+    @ResponseBody
+    public String myForm(@RequestParam String name,@RequestParam int age) {
+        return name + "  " + age;
     }
 }
