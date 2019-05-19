199cbeaf37cdcc3624420d43086446adf1af2484
Plekhanov <atagan@rambler.ru>
2019-05-19 21:03:34 +0300

    добавил sqlite

--- JDBC/src/main/java/JDBC/SQLite.java
+++ JDBC/src/main/java/JDBC/SQLite.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 /**
  * База SQLite  lessons.db
  */
@@ -13,12 +14,12 @@ public class SQLite {
 
     private static final String FS = File.separator;
     private static final String URL = "jdbc:sqlite:JDBC" + FS + "src" + FS + "main" + FS + "resources" + FS + "lessons.db";
-
     private static Connection connection;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         connection = DriverManager.getConnection(URL);
+        System.out.println("Подключение к базе");
 
         dropTableIfExists();
         createTable();
