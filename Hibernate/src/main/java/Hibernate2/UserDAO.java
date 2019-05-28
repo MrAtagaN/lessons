4f6b6aec3f75494ea0c76760f16798d71811f8bc
plekhanov <atagan@rambler.ru>
2019-05-28 22:26:19 +0300

    рефактор Hibernate

--- Hibernate/src/main/java/Hibernate2/UserDAO.java
+++ Hibernate/src/main/java/Hibernate2/UserDAO.java
@@ -1,19 +1,42 @@
 package Hibernate2;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * HQL
+ */
 public class UserDAO {
 
     private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    public List<User> getAllUsers() {
+        List<User> result = new ArrayList();
+        Session session = sessionFactory.openSession();
+        try {
+            Query query = session.createQuery("FROM user");
+            result = query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return result;
+    }
 
     public User findById(int id) {
         User result = null;
         Session session = sessionFactory.openSession();
         try {
-            result = (User) sessionFactory.openSession().get(User.class, id);
+            result = (User) session.get(User.class, id);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
