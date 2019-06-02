0cc2c820ffec5e2a6c701d74d95f582ad01476a0
plekhanov <atagan@rambler.ru>
2019-06-02 23:39:02 +0300

    рефактор

--- WebService/Frontend/src/app/app.component.ts
+++ WebService/Frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import{Component}from'@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui';
+  page = 1;
+
+    changeTitle1() {
+      this.page = 1;
+    }
+
+    changeTitle2() {
+      this.page = 2;
+    }
 }
