936c0950d75a79ebb21402154d0ea56a209023bf
Plekhanov <atagan@rambler.ru>
2019-06-05 22:15:09 +0300

    Работа в Frontend

--- WebService/Frontend/src/app/app.module.ts
+++ WebService/Frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './pages/page1/page1.component';
 import { Page2Component } from './pages/page2/page2.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
-    Page2Component
+    Page2Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
