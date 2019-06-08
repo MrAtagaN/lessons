7c55ef1f656742976bf4efa60e9d8af6a8309405
plekhanov <atagan@rambler.ru>
2019-06-08 18:14:49 +0300

    работа в SpringData

--- SpringData/src/main/java/SpringData/Repository/PersonRepository.java
+++ SpringData/src/main/java/SpringData/Repository/PersonRepository.java
@@ -1,11 +1,14 @@
 package SpringData.Repository;
 
+import SpringData.Entities.Address;
 import SpringData.Entities.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -20,6 +23,10 @@ public class PersonRepository {
 
     public List<Person> getAllUsers() {
         return jdbcTemplate.query("select * from PERSON join ADDRESS on PERSON.address_id = ADDRESS.id", rowMapper);
+    }
+
+    public void createUser(Person person) {
+        jdbcTemplate.update("insert into PERSON (name, age, phone, address_id, birthday) values (?, ?, ?, ?, ?)", person.getName(), person.getAge(), person.getPhone(), person.getAddress().getId(), person.getBirthday());
     }
 }
 
