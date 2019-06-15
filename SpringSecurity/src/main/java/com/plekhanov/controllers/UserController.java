ff029907f4e5663624995a0dd775e8f7e5a0c03e
Plekhanov <atagan@rambler.ru>
2019-06-15 23:30:39 +0300

    работа в  SpringSecurity

--- SpringSecurity/src/main/java/com/plekhanov/controllers/UserController.java
+++ SpringSecurity/src/main/java/com/plekhanov/controllers/UserController.java
@@ -1,8 +1,11 @@
 package com.plekhanov.controllers;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.plekhanov.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +18,9 @@ public class UserController {
     User user;
 
     @GetMapping(value = "/getUser")
-    public String getUser() {
-        return "test";
+    public String getUser() throws JsonProcessingException {
+
+        ObjectMapper mapper = Jackson2ObjectMapperBuilder.json().build();
+        return mapper.writeValueAsString(user);
     }
 }
