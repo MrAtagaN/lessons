467977d81ba842173002d95b995a1aca226d712b
Plekhanov <atagan@rambler.ru>
2019-06-15 23:50:17 +0300

    работа в  SpringSecurity

--- SpringSecurity/src/main/java/com/plekhanov/config/ConfigMapper.java
+++ SpringSecurity/src/main/java/com/plekhanov/config/ConfigMapper.java
@@ -1,11 +1,7 @@
 package com.plekhanov.config;
 
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -16,23 +12,14 @@ import java.util.List;
 @Configuration
 public class ConfigMapper extends WebMvcConfigurationSupport {
 
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
-
-        return mapper;
-    }
-
     @Override
     protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 
-        converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
+        converters.add(new MappingJackson2HttpMessageConverter());
+        //converters.add(new MappingJackson2HttpMessageConverter(objectMapper)); //Можно добавить кастомный  ObjectMapper
     }
+
+
 
 
 }
