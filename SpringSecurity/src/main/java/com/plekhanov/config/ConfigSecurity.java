9de463286daf0cb160fc76ae3bc81d020de2f7fb
Plekhanov <atagan@rambler.ru>
2019-06-16 15:21:48 +0300

    работа в  SpringSecurity

--- SpringSecurity/src/main/java/com/plekhanov/config/ConfigSecurity.java
+++ SpringSecurity/src/main/java/com/plekhanov/config/ConfigSecurity.java
@@ -2,11 +2,28 @@ package com.plekhanov.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 
 @Configuration
 @EnableWebSecurity
 public class ConfigSecurity extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().formLogin();
+    }
 }
