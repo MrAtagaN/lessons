48bdbfe0801e39bb5b2a9ff8d7f785978d8532f9
Plekhanov <atagan@rambler.ru>
2019-06-21 22:47:16 +0300

    работа в MailClient

--- JavaCore/src/main/java/mailClient/Main.java
+++ JavaCore/src/main/java/mailClient/Main.java
@@ -1,6 +1,7 @@
 package mailClient;
 
 
+import javax.mail.MessagingException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,12 +9,12 @@ import java.io.InputStreamReader;
 public class Main {
 
     private static final String USER_NAME = "atagan@rambler.ru";
-    private static final String HOST = "imap.rambler.ru";
+    private static final String IMAP_HOST = "imap.rambler.ru";
     private static final String MAIL_STORE_TYPE = "imaps";
-    private static final int PORT = 993;
+    private static final int IMAP_PORT = 993;
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, MessagingException {
 
         //считываем пароль от mail
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -21,9 +22,13 @@ public class Main {
         String password = reader.readLine();
         reader.close();
 
-        MailClient mailClient = new MailClient(HOST, PORT, USER_NAME, MAIL_STORE_TYPE, password);
-
+        //чтение почты
+        MailClient mailClient = new MailClient(IMAP_HOST, IMAP_PORT, USER_NAME, MAIL_STORE_TYPE, password);
         mailClient.readMessages("INBOX",20);
         mailClient.saveMessage("INBOX",11);
+
+        //отправка почты
+        MailSender mailSender = new MailSender("smtp.rambler.ru", 465,  "atagan@rambler.ru", USER_NAME, password);
+        mailSender.sendMail("atagan@rambler.ru");
     }
 }
