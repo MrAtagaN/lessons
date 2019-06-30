df57eaced08e1c48bd90b21f7bc1b88add76ee83
Plekhanov <atagan@rambler.ru>
2019-06-30 19:32:49 +0300

    работа в references

---
+++ JavaCore/src/main/java/collections/Main.java
@@ -0,0 +1,51 @@
+package collections;
+
+
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+
+/**
+ * Реализации {@link Collection},  {@link Iterable}
+ *
+ * Методы Iterable:
+ *
+ * Iterator<T> iterator();
+ * Spliterator<T> spliterator();
+ * void forEach(Consumer<? super T> action)
+ */
+public class Main {
+
+    /***
+     * Реализации {@link List}
+     */
+    List arrayList = new ArrayList<>(); //List
+    List copyOnWriteArrayList = new CopyOnWriteArrayList(); //List
+    List linkedList = new LinkedList<>(); //List ,Queue, Deque
+    List vector = new Vector(); //List
+    List stack = new Stack(); //List
+
+
+    /**
+     * Set
+     */
+    Set hashSet = new HashSet(); //Set
+    Set linkedHashSet = new LinkedHashSet(); //Set
+
+
+    /**
+     * Реализации {@link Queue}
+     */
+    Queue linkedList2 = new LinkedList<>(); //List ,Queue, Deque
+    Queue priorityQueue = new PriorityQueue(); //Queue
+    Queue arrayDeque = new ArrayDeque(); //Queue, Deque
+
+
+    /**
+     * Реализации {@link Deque}
+     */
+    Deque arrayDeque2 = new ArrayDeque(); //Queue, Deque
+    Deque linkedList3 = new LinkedList<>(); //List ,Queue, Deque
+
+
+}
