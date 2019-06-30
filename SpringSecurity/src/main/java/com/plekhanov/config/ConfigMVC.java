d8765273ae48423eb52141cf9331db6efb5caa92
Plekhanov <atagan@rambler.ru>
2019-06-30 21:38:19 +0300

    рефактор

--- SpringSecurity/src/main/java/com/plekhanov/config/ConfigMVC.java
+++ SpringSecurity/src/main/java/com/plekhanov/config/ConfigMVC.java
@@ -1,15 +1,17 @@
 package com.plekhanov.config;
 
-
-
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import java.util.List;
 
 
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.plekhanov")
 public class ConfigMVC extends WebMvcConfigurationSupport {
 
     @Override
