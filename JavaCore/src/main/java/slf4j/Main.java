f57655f6168cf976c2d197bff88f532105a8f2df
Plekhanov <atagan@rambler.ru>
2019-07-01 22:21:53 +0300

    logger

--- JavaCore/src/main/java/slf4j/Main.java
+++ JavaCore/src/main/java/slf4j/Main.java
@@ -1,6 +1,16 @@
 package slf4j;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+
+    public static Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        LOG.info("SOME MESSAGE");
+
+    }
 }
