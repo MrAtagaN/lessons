1465d46f84bc6a6b5b0125bad5042727399ee544
Plekhanov <atagan@rambler.ru>
2019-07-02 23:32:57 +0300

    Cron

---
+++ JavaCore/src/main/java/libs/cron/ScheduleTask.java
@@ -0,0 +1,32 @@
+package libs.cron;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleTask {
+
+    public static Logger LOG = LoggerFactory.getLogger(ScheduleTask.class);
+
+    /**
+     * 1.Seconds
+     *
+     * 2.Minutes
+     *
+     * 3.Hours
+     *
+     * 4.Day-оф-Месяц
+     *
+     * 5.Month
+     *
+     * 6.Day-недельного
+     *
+     * 7.Year(необязательное поле)
+     */
+    @Scheduled(cron="* * * * * *", zone="Europe/Istanbul")
+    public void doScheduledWork() {
+        LOG.info("DO SCHEDULED WORK");
+    }
+}
