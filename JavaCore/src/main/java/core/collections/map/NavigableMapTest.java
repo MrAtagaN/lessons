7b5ae61a48b60cfc8df7b60f30f7a2c14b6146ec
Plekhanov <atagan@rambler.ru>
2019-07-20 23:54:32 +0300

    collections

---
+++ JavaCore/src/main/java/core/collections/map/NavigableMapTest.java
@@ -0,0 +1,37 @@
+package core.collections.map;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+
+/**
+ * descendingMap - развернутая Map
+ *
+ * ceilingEntry - ближайший Entry в большую сторону
+ * floorEntry - ближайший Entry в меньшую сторону
+ * higherEntry - строго больший Entry
+ * lowerEntry - строго меньший Entry
+ *
+ * pollFirstEntry - убирает первый Entry
+ * pollLastEntry - убирает последний Entry
+ *
+ * subMap - возвращает часть Map
+ * headMap - возвращает Map идущий до определенного числа
+ * tailMap - возвращает Map идущий после определенного числа
+ */
+public class NavigableMapTest {
+
+    public static void main(String[] args) {
+        NavigableMap<Integer, String> map = new TreeMap<>();
+
+        map.put(1, "a");
+        map.put(2, "a");
+        map.put(3, "a");
+        map.put(4, "a");
+        map.put(5, "a");
+
+    }
+}
+
+
