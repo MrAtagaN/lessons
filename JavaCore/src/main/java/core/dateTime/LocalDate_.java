45bbf3fa0c09892072d56b45d578b4b55da9d237
Plekhanov <atagan@rambler.ru>
2019-07-27 02:37:03 +0300

    рефактор

--- JavaCore/src/main/java/core/dateTime/LocalDate_.java
+++ JavaCore/src/main/java/core/dateTime/LocalDate_.java
@@ -1,16 +1,55 @@
 package core.dateTime;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class LocalDate_ {
 
     public static void main(String[] args) {
-        LocalDate localDate = LocalDate.now();
-        System.out.println("localDate = " + localDate);
-
         LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println("localDateTime = " + localDateTime);
+        LocalDate.of(2012, Month.DECEMBER, 12);
+        LocalDate.ofEpochDay(150);  // middle of 1970
+        LocalTime.of(17, 18);
+        LocalTime.parse("10:15:30");
 
+        //Геттеры
+        LocalDate theDate = localDateTime.toLocalDate();
+        Month month = localDateTime.getMonth();
+        int day = localDateTime.getDayOfMonth();
+        localDateTime.getSecond();
+
+        //Установка значения, возвращает новый объект
+        LocalDateTime thePast = localDateTime.withDayOfMonth(10).withYear(2010);
+
+        /* You can use direct manipulation methods,
+        or pass a value and field pair */
+        LocalDateTime yetAnother = thePast.plusWeeks(3).plus(3, ChronoUnit.WEEKS);
+
+        //Сокращения
+        LocalDateTime truncatedTime = localDateTime.truncatedTo(ChronoUnit.SECONDS);
+
+        // You can specify the zone id when creating a zoned date time
+        ZoneId zoneId = ZoneId.of("Europe/Paris");
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, zoneId);
+        System.out.println(zoneId.equals(ZoneId.from(zonedDateTime)));
+
+
+        //Переоды
+        // 3 years, 2 months, 1 day
+        Period period = Period.of(3, 2, 1);
+
+        // You can modify the values of dates using periods
+        LocalDate oldDate = LocalDate.now();
+        LocalDateTime oldDateTime = LocalDateTime.now();
+
+        LocalDate newDate = oldDate.plus(period);
+        ZonedDateTime newDateTime = zonedDateTime.minus(period);
+        // Components of a Period are represented by ChronoUnit values
+        System.out.println(period.get(ChronoUnit.DAYS) == 1);
+
+
+        //Продолжительность, различная точность
+        Duration duration = Duration.between(truncatedTime, oldDateTime);
+        System.out.println("oneDay = " + duration);
     }
 }
--- JavaCore/src/main/java/core/dateTime/Main.java
+++ JavaCore/src/main/java/core/dateTime/Main.java
@@ -1,6 +1,8 @@
 package core.dateTime;
 
 import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 
 
@@ -9,5 +11,8 @@ public class Main {
     public static void main(String[] args) {
         LocalDateTime localDateTime = LocalDateTime.now();
         Timestamp timestamp = Timestamp.valueOf(localDateTime);
+
+        DateFormat instance = SimpleDateFormat.getInstance();
+        instance.format("");
     }
 }
