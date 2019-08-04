99a77469956802a60080ee41018add16d5d7312f
Plekhanov <atagan@rambler.ru>
2019-08-04 17:15:32 +0300

    Mercurial

--- JavaCore/src/main/java/core/string/Main.java
+++ JavaCore/src/main/java/core/string/Main.java
@@ -1,4 +1,15 @@
 package core.string;
 
 public class Main {
+
+    public static void main(String[] args) {
+        String a ="Hello";
+        final String b ="Hel";
+        String c ="lo";
+        String d ="Hello";
+
+        System.out.println(a == d);
+        System.out.println(a == b + c);
+        System.out.println(a == b + "lo");
+    }
 }
