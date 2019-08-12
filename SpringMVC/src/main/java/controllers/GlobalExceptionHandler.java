c29c9e9823afa545cf58fb4817873b422a92f379
Plekhanov <atagan@rambler.ru>
2019-08-13 00:10:22 +0300

    Транзакции

--- SpringMVC/src/main/java/controllers/GlobalExceptionHandler.java
+++ SpringMVC/src/main/java/controllers/GlobalExceptionHandler.java
@@ -4,16 +4,16 @@ import model.MyException;
 import model.MyException2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * Обработчик ошибок, выбрасываемых из контроллеров
  *
  * https://spring.io/blog/2013/11/01/exception-handling-in-spring-mvc
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     /**
