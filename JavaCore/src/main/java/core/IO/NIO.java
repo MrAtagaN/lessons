4b9ee3e1f7937bffa94a8de2e8d9cfae1b63b71d
Plekhanov <atagan@rambler.ru>
2019-09-29 23:17:14 +0300

    IO

---
+++ JavaCore/src/main/java/core/IO/NIO.java
@@ -0,0 +1,37 @@
+package core.IO;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * NIO - Files, Path, Paths
+ *
+ * https://javarush.ru/quests/lectures/questcollections.level01.lecture03
+ */
+public class NIO {
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("TODO.txt");
+
+        //Path
+        System.out.println(path.isAbsolute());
+        System.out.println(path.getRoot());
+        System.out.println(path.getFileName());
+        System.out.println(path.getParent());
+        System.out.println(path.startsWith(path));
+        System.out.println(path.endsWith(path));
+        System.out.println(path.normalize());
+        System.out.println(path.relativize(path));
+        System.out.println(path.toAbsolutePath());
+        System.out.println(path.toFile());
+
+        //Files
+        Files.readAllBytes(path);
+
+        List<String> lines = Files.readAllLines(path);
+        lines.forEach(System.out::println);
+    }
+}
---
+++ JavaCore/src/main/java/core/IO/OutputStream_.java
@@ -0,0 +1,4 @@
+package core.IO;
+
+public class OutputStream_ {
+}
