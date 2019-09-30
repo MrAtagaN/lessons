6d29a1b71b6c5b9bdeee06a4b51d12959b98ea82
Plekhanov <atagan@rambler.ru>
2019-09-30 22:24:33 +0300

    Паттерны

--- JavaCore/src/main/java/patterns/structural/Decorator.java
+++ JavaCore/src/main/java/patterns/structural/Decorator.java
@@ -1,5 +1,8 @@
 package patterns.structural;
 
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
 /**
  * (Wrapper)
  * Декоратор оборачивает некоторый класс и предоставляет такой же или расширенный интерфейс.
@@ -7,4 +10,12 @@ package patterns.structural;
  * оригинальным классом и при этом расширять его функциональность.
  */
 public class Decorator {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+
+        //BufferedInputStream - декоратор
+        InputStream inputStream2 = new BufferedInputStream(System.in);
+
+    }
 }
