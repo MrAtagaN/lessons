d78512a422479d03e788b3427321d99a2de9d71d
Plekhanov <atagan@rambler.ru>
2019-10-28 23:07:44 +0300

    алгоритмы

---
+++ JavaCore/src/main/java/core/multithreading/locks/ReentrantLock_.java
@@ -0,0 +1,28 @@
+package core.multithreading.locks;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class ReentrantLock_ {
+
+    public static void main(String[] args) throws InterruptedException {
+        Lock lock = new ReentrantLock();
+        try {
+            lock.lock();
+
+            boolean tryLock = lock.tryLock(1000, TimeUnit.MICROSECONDS);
+            System.out.println(tryLock);
+
+            Condition condition = lock.newCondition();
+            condition.await(); //аналог wait
+            condition.signal(); //аналог notify
+
+
+        } finally {
+            lock.unlock();
+        }
+    }
+}
