95640b05b96c440d7184eaa2fc1f4c9704c4a765
plekhanov <atagan@rambler.ru>
2020-01-02 03:40:12 +0300

    Hibernate

--- Hibernate/src/main/java/theory/transactions/DataManagment.java
+++ Hibernate/src/main/java/theory/transactions/DataManagment.java
@@ -3,20 +3,30 @@ package theory.transactions;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
+import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import java.util.Set;
 
 /**
  *
  * @LazyCollection(LazyCollectionOption.EXTRA) - Поддержка операций с коллекцией не вызывающий ее инициализацию.
- *  size, isEmpty, contains. Список List загрузит только один элемент при вызове get(index).
+ * size, isEmpty, contains. Список List загрузит только один элемент при вызове get(index).
+ *
+ *
+ *  Проблема N + 1
+ *
+ *  Решения:
+ *  @OneToMany(fetch = FetchType.EAGER) - По умолчанию использует стратегию JOIN. Работает для связей @OneToMany @OneToOne
+ *  если извлекается одна коллекция, иначе возникает проблема декартова произведения
  *
  *
  */
 
 public class DataManagment {
 
-    @OneToMany
+
+    @OneToMany(fetch = FetchType.EAGER)
     @LazyCollection(LazyCollectionOption.EXTRA)
     private Set<String> stringSet;
 }
