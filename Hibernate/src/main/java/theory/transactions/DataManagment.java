f57f0b687eac987e6fd1b6e482d7b1e3bca0d76c
plekhanov <atagan@rambler.ru>
2020-01-04 00:00:09 +0300

    Hibernate

--- Hibernate/src/main/java/theory/transactions/DataManagment.java
+++ Hibernate/src/main/java/theory/transactions/DataManagment.java
@@ -1,11 +1,11 @@
 package theory.transactions;
 
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
+import org.hibernate.annotations.*;
 
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -20,13 +20,19 @@ import java.util.Set;
  *  @OneToMany(fetch = FetchType.EAGER) - По умолчанию использует стратегию JOIN. Работает для связей @OneToMany @OneToOne
  *  если извлекается одна коллекция, иначе возникает проблема декартова произведения
  *
+ *  @BatchSize(size = 10) - При загрузке одного прокси объекта, может быть загружено до 10 объектов. Аннотация ставится
+ *  над коллекцией или над классом
  *
+ *  @Fetch(FetchMode.SUBSELECT) - Как только инициализируется одна из коллекций, Hibernate инициализирует все коллекции.
+ *  На момент написания доступна только для коллекций с планом отложенной загрузки, но не для прокси-обектов сущностей.
  */
 
+@BatchSize(size = 10)
 public class DataManagment {
-
 
     @OneToMany(fetch = FetchType.EAGER)
     @LazyCollection(LazyCollectionOption.EXTRA)
-    private Set<String> stringSet;
+    @BatchSize(size = 10)
+    @Fetch(FetchMode.SUBSELECT)
+    private Set<String> stringSet= new HashSet<>();
 }
