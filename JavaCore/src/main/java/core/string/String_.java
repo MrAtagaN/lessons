27e706e47a947d6fd9d758923d21f03c59bb405a
PlekhanovSA <SAPlekhanov@sberbank.ru>
2020-03-27 23:35:12 +0300

    docker

--- JavaCore/src/main/java/core/string/String_.java
+++ JavaCore/src/main/java/core/string/String_.java
@@ -1,5 +1,12 @@
 package core.string;
 
+/**
+ * String.copyValueOf() -
+ * String.format() -
+ * String.join() - соединяет строки, вставляя между ними разделитель
+ *
+ *
+ */
 public class String_ {
 
     public static void main(String[] args) {
@@ -8,8 +15,29 @@ public class String_ {
         String c ="lo";
         String d ="Hello";
 
-        System.out.println(a == d);
-        System.out.println(a == b + c);
-        System.out.println(a == b + "lo");
+        System.out.println(a == d); //true
+        System.out.println(a == b + c); //false
+        System.out.println(a == b + "lo"); //true
+
+//        String.copyValueOf()
+//        String.format()
+//        String.join()
+
+//        a.charAt()
+//        a.codePointAt()
+//        a.codePointBefore()
+//        a.codePointCount()
+//        a.codePoints()
+//        a.compareTo()
+//        a.compareToIgnoreCase()
+//        a.concat()
+//        a.contains()
+//        a.intern()
+//        a.matches()
+//        a.offsetByCodePoints()
+//        a.replace()
+//        a.split()
+//        a.substring()
+
     }
 }
