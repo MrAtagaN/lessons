ee4d58af29ec040074cdb26c4f29972f120ed483
Plekhanov <atagan@rambler.ru>
2020-05-31 22:03:40 +0300

    refactor

--- JavaCore/src/main/java/core/references/softReferences/SoftReference_.java
+++ JavaCore/src/main/java/core/references/softReferences/SoftReference_.java
@@ -3,7 +3,7 @@ package core.references.softReferences;
 import java.lang.ref.SoftReference;
 
 /**
- * {@link SoftReference}
+ * {@link SoftReference} - Удаляются при нехватке памяти
  *
  *
  * https://javarush.ru/quests/lectures/questcollections.level04.lecture03
--- JavaCore/src/main/java/core/references/weakHashMap/WeakHashMapTest.java
+++ JavaCore/src/main/java/core/references/weakHashMap/WeakHashMapTest.java
@@ -4,9 +4,8 @@ import java.util.Map;
 import java.util.WeakHashMap;
 
 /**
- * {@link WeakHashMap}
- * Элементы из WeakHashMap удаляются при каждой сборке мусора
- *
+ * {@link WeakHashMap} - Элементы из WeakHashMap удаляются при каждой сборке мусора
+ * <p>
  * https://javarush.ru/quests/lectures/questcollections.level04.lecture05
  */
 public class WeakHashMapTest {
@@ -15,13 +14,13 @@ public class WeakHashMapTest {
         Map<SomeData, Integer> map = new WeakHashMap<>();
 
         SomeData someData = new SomeData();
-        map.put(someData,90);
+        map.put(someData, 90);
 
         someData = null;
         System.gc();
 
         for (int i = 0; i < 1000; i++) {
-            if(map.isEmpty()) {
+            if (map.isEmpty()) {
                 System.out.println("MAP IS EMPTY!");
                 break;
             } else {
