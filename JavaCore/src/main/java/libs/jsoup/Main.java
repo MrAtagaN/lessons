878e6761b47eb961026814041e621498839a272a
PlekhanovSA <SAPlekhanov@sberbank.ru>
2020-06-04 17:48:17 +0300

    jsoup

--- JavaCore/src/main/java/libs/jsoup/Main.java
+++ JavaCore/src/main/java/libs/jsoup/Main.java
@@ -10,6 +10,33 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        parseTestPage();
+        //parseHH();
+
+
+    }
+
+
+    private static void parseTestPage() throws IOException {
+        File file = new File("JavaCore/src/main/resources/hh-test.html");
+        Document document = Jsoup.parse(file, "UTF-8", "hh.ru");
+
+//        getElementsByTag
+//                findFirstElementByTagName
+
+
+        Elements titleElem = document.select("head > title");
+        System.out.println(titleElem.text());
+
+        Elements contentElem = document.select("body > div.content");
+        System.out.println("content:\n" + contentElem.text());
+        System.out.println("content:\n" + contentElem.html());
+    }
+
+
+
+
+    private static void parseHH() throws IOException {
         File input = new File("JavaCore/src/main/resources/hh.html");
         Document document = Jsoup.parse(input, "UTF-8", "hh.ru");
 
@@ -17,11 +44,8 @@ public class Main {
 
         Elements title = document.select("head > title");
         System.out.println("title = " + title.html());
-
-
-
-
-
-
     }
+
+
+
 }
