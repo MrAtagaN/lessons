aed072cb0948f28f2fdbf3760cb2f741bb5e682c
Plekhanov <atagan@rambler.ru>
2020-09-27 04:14:24 +0300

    react todo application

--- JavaScript/src/main/resources/React/test-react-app/src/todo/TodoList.js
+++ JavaScript/src/main/resources/React/test-react-app/src/todo/TodoList.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import TodoItem from './TodoItem.js';
+import PropTypes from 'prop-types';
 
-export default function TodoList(args) {
+
+const styles = {
+    ul: {listStyle: 'none', margin: 0, padding: 0}
+};
+
+function TodoList(args) {
     return (
-        <ul>
-            {args.todos.map((todo, index) => {return <TodoItem todo={todo} key={todo.id} index={index}/>} )}
+        <ul style={styles.ul}>
+            {args.todos.map((todo, index) => {
+                return <TodoItem
+                    todo={todo}
+                    key={todo.id} // добавляем рекомендуемое значения key для итерируемых объектов в react
+                    index={index}
+                    onToggle={args.onToggle}/>
+            })}
         </ul>
     )
 }
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onToggle: PropTypes.func.isRequired
+};
+
+export default TodoList;
+
