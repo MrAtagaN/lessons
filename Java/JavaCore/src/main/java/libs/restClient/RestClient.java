b774f971183a300c64e581917d3e7a887b45085a
PlekhanovSA <SAPlekhanov@sberbank.ru>
2020-09-27 19:21:43 +0300

    rest client

--- Java/JavaCore/src/main/java/libs/restClient/RestClient.java
+++ Java/JavaCore/src/main/java/libs/restClient/RestClient.java
@@ -1,9 +1,19 @@
 package libs.restClient;
 
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
 
@@ -30,7 +40,7 @@ public class RestClient {
     /**
      * Post request
      */
-    public ResponseEntity<String> post(String url, String body,  Map<String, List<String>> headers) {
+    public ResponseEntity<String> post(String url, String body, Map<String, List<String>> headers) {
         if (headers == null) {
             headers = this.httpHeaders;
         }
@@ -41,16 +51,15 @@ public class RestClient {
     }
 
 
-
     /**
      * Add headers to another headers
      *
-     * @param headers headers to add
+     * @param headers     headers to add
      * @param httpHeaders target headers
      */
     public void addHeaders(Map<String, List<String>> headers, HttpHeaders httpHeaders) {
         if (headers != null && httpHeaders != null) {
-            headers.forEach( (nameValue, values) -> {
+            headers.forEach((nameValue, values) -> {
                 values.forEach(value -> {
                     httpHeaders.add(nameValue, value);
                 });
@@ -65,5 +74,29 @@ public class RestClient {
         System.out.println("Body: " + responseEntity.getBody());
     }
 
+
+    /**
+     * RestTemplate доверяющий всем сертификатам
+     */
+    public RestTemplate restTemplate()
+            throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStrategy)
+                .build();
+
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+        requestFactory.setHttpClient(httpClient);
+        return new RestTemplate(requestFactory);
+
+    }
 
 }
