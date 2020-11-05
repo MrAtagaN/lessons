985657acd8ac3546ba6ed3cc56b1e2ff07ae2fb7
Plekhanov <atagan@rambler.ru>
2020-11-05 20:02:51 +0300

    refactor

---
+++ Java/Core/src/main/java/core/streamAPI/test.java
@@ -0,0 +1,16 @@
+package core.streamAPI;
+
+public class test {
+
+
+    public static void main(String[] args) {
+
+        String a = "ab";
+        String aa = "a";
+        String bb = "b";
+
+        String b = aa + bb;
+
+        System.out.println(a == b);
+    }
+}
