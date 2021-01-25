69f377bc66dcb9ace8ee2f99646d87ec00b6732c
Plekhanov <atagan@rambler.ru>
2021-01-26 01:10:19 +0300

    restTemplate

--- Java/Spring/Web/src/main/java/com/plekhanov/httpClient/HttpClient_.java
+++ Java/Spring/Web/src/main/java/com/plekhanov/httpClient/HttpClient_.java
@@ -1,11 +1,22 @@
 package com.plekhanov.httpClient;
 
+import org.apache.http.HttpRequest;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
 
 /**
  *
  * Методы {@link HttpClientBuilder}
+ *
+ * Методы {@link HttpUriRequest}
+ * Методы {@link HttpRequest}
+ *
+ * Методы {@link HttpContext}
+ *
  *
  * Методы {@link CloseableHttpClient}
  *
@@ -13,7 +24,10 @@ import org.apache.http.impl.client.HttpClientBuilder;
 public class HttpClient_ {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        final CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .build();
 
+        httpClient.execute(null);
     }
 }
