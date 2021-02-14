cf82b320ef4d0be0946bea9941ea4371ac2e39fd
Plekhanov <atagan@rambler.ru>
2021-02-15 01:06:11 +0300

    streamApi

---
+++ Java/Core/src/main/java/core/streamAPI/CustomCollector.java
@@ -0,0 +1,72 @@
+package core.streamAPI;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * Для создания своего коллектора нужно заимплементить интерфейс {@link Collector}
+ *
+ * T – тип который возможен в коллекции
+ * A – тип временного объекта в который будет накапливаться элементы
+ * R – тип контейнера с результатом
+ *
+ * supplier -
+ * accumulator -
+ * combiner -
+ * finisher -
+ * characteristics -
+ */
+public class CustomCollector<T> implements Collector<T, Set<T>, Set<T>> {
+
+    @Override
+    public Supplier<Set<T>> supplier() {
+        return HashSet::new;
+    }
+
+    @Override
+    public BiConsumer<Set<T>, T> accumulator() {
+        return Set::add;
+    }
+
+    @Override
+    public BinaryOperator<Set<T>> combiner() {
+        return (left, right) -> {
+            left.addAll(right);
+            return left;
+        };
+    }
+
+    @Override
+    public Function<Set<T>, Set<T>> finisher() {
+        return (x) -> x;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return new HashSet<>();
+    }
+
+    public static <T> CustomCollector<T> toSet() {
+        return new CustomCollector<>();
+    }
+
+
+    /**
+     * Example of work
+     */
+    public static void main(String[] args) {
+        List<String> givenList = Arrays.asList("a", "bb", "ccc", "dddd");
+
+        Set<String> result = givenList.stream()
+                .collect(toSet());
+
+    }
+}
--- Java/Libs/Guava/src/main/java/com/plekhanov/Main.java
+++ Java/Libs/Guava/src/main/java/com/plekhanov/Main.java
@@ -1,11 +1,16 @@
 package com.plekhanov;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
+
+import java.util.stream.Collector;
 
 
 public class Main {
 
     public static void main(String[] args) {
+        Sets.immutableEnumSet(Collector.Characteristics.UNORDERED);
+
 
         ImmutableMap.<String, String>builder()
                 .put("KEY","VALUE")
