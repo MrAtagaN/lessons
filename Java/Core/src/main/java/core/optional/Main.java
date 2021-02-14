bc7bba55d8041559d68cd2b29aaea765e6695d58
Plekhanov <atagan@rambler.ru>
2021-02-14 21:18:21 +0300

    optional

--- Java/Core/src/main/java/core/optional/Main.java
+++ Java/Core/src/main/java/core/optional/Main.java
@@ -10,32 +10,56 @@ import java.util.OptionalLong;
  * https://vk.com/@javatutorial-java-optional-ne-takoi-uzh-ochevidnyi
  *
  *
+ * Методы {@link Optional}:
+ *
+ * Создание:
+ * empty - создает Optional c значением null
+ * of - создает Optional c переданным значением, не равным null
+ * ofNullable - создает Optional c переданным значением
+ *
+ * Получение:
+ * get - возвращает результат, или кидает ошибку
+ * orElse - возвращает результат, или значение по умолчанию
+ * orElseGet - возвращает результат, результат функции
+ * orElseThrow - возвращает результат, или кидает ошибку
+ *
+ * Действия:
+ * isPresent - проверка на null
+ * filter - фильтрует по предикату
+ * map - преобразует по функции
+ * flatMap - преобразует по функции. Функция должна оборачивать результат в Optional
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //создание
         Optional<Object> empty = Optional.empty();
+        Optional<String> optionalString = Optional.of("qwe");
+        OptionalInt.of(4);
+        OptionalDouble.of(2.3);
+        OptionalLong.of(12);
+        Optional.ofNullable(null);
 
-        empty.isPresent();
+
+        //получение
+        optionalString.get();
         empty.orElse("asd");
         empty.orElseGet(() -> {
             return 2 + 2;
         });
-
-        Optional<String> optionalString = Optional.of("qwe");
-        optionalString.get();
-
-        String string = null;
-        Optional.ofNullable(string).filter(elem -> {
-            return elem.contains("a");
+        optionalString.orElseThrow(() -> {
+            return new Exception();
         });
 
-        OptionalInt.of(4);
-        OptionalDouble.of(2.3);
-        OptionalLong.of(12);
 
+        //действия
+        empty.isPresent();
+        empty.filter(elem -> {
+            return ((String) elem).contains("a");
+        });
 
-
+        optionalString.map((elem) -> {return elem + elem;});
+        optionalString.flatMap((elem) -> {return Optional.of(elem + elem); });
 
     }
 }
