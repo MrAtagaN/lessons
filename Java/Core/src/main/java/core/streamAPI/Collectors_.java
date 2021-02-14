3037acd62acbc7d385aee0d93d51bad5977eab5c
Plekhanov <atagan@rambler.ru>
2021-02-15 00:27:00 +0300

    streamApi

--- Java/Core/src/main/java/core/streamAPI/Collectors_.java
+++ Java/Core/src/main/java/core/streamAPI/Collectors_.java
@@ -1,12 +1,11 @@
 package core.streamAPI;
 
 
-import java.util.Arrays;
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Collector;
 
+import static java.util.stream.Collectors.*;
 import static java.util.stream.Collectors.summarizingDouble;
 
 /**
@@ -19,12 +18,12 @@ import static java.util.stream.Collectors.summarizingDouble;
  * averagingInt -
  * collectingAndThen - собрать в коллекцию, затем преобразовать
  * counting - количество элементов в стриме
- * groupingBy -
+ * groupingBy - группирует элементы в Map, где ключ - значение определенной функции
+ * partitioningBy - группирует элементы в Map, где ключ - значение определенного предиката
  * groupingByConcurrent -
  * joining - соединяет строки
  * mapping -
- * maxBy -
- * partitioningBy -
+ * maxBy - максимальное значение
  * reducing -
  * summarizingDouble - возвращает статистику об элементах в стриме {@link DoubleSummaryStatistics}
  * summingInt - сумма элементов
@@ -39,13 +38,20 @@ public class Collectors_ {
     public static void main(String[] args) {
         List<String> givenList = Arrays.asList("a", "bb", "ccc", "dd");
 
-        DoubleSummaryStatistics result = givenList.stream().collect(summarizingDouble(String::length));
+        DoubleSummaryStatistics summarizingDouble = givenList.stream()
+                .collect(summarizingDouble(String::length));
 
-        System.out.println(result.getAverage()); //2.0
-        System.out.println(result.getCount()); //4
-        System.out.println(result.getMax()); //3.0
-        System.out.println(result.getMin()); //1.0
-        System.out.println(result.getSum()); //8.0
+        System.out.println(summarizingDouble.getAverage()); //2.0
+        System.out.println(summarizingDouble.getCount()); //4
+        System.out.println(summarizingDouble.getMax()); //3.0
+        System.out.println(summarizingDouble.getMin()); //1.0
+        System.out.println(summarizingDouble.getSum()); //8.0
+
+        Map<Integer, Set<String>> groupingBy = givenList.stream()
+                .collect(groupingBy(String::length, toSet()));
+
+        Map<Boolean, List<String>> partitioningBy = givenList.stream()
+                .collect(partitioningBy(s -> s.length() > 2));
 
 
     }
