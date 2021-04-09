82e55ce0735d22ad14762cfd5fb2e6bf385b1154
Plekhanov <atagan@rambler.ru>
2021-04-10 01:14:25 +0300

    tests

---
+++ Java/Tests/Mockito/src/main/java/com/plekhanov/service/RestService.java
@@ -0,0 +1,19 @@
+package com.plekhanov.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
+public class RestService {
+
+    RestTemplate restTemplate;
+
+    public RestService (RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String doGet(String url) {
+        ResponseEntity entity = restTemplate.getForEntity(url, String.class);
+        return entity.toString();
+    }
+}
