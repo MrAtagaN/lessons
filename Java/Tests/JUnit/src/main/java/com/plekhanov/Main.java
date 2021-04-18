583b15b94129b64c73401bcba40a1e728795da90
Plekhanov <atagan@rambler.ru>
2021-04-18 21:51:11 +0300

    maven

--- Java/Tests/JUnit/src/main/java/com/plekhanov/Main.java
+++ Java/Tests/JUnit/src/main/java/com/plekhanov/Main.java
@@ -3,6 +3,9 @@ package com.plekhanov;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://www.marcphilipp.de/downloads/posts/2013-01-02-hamcrest-quick-reference/Hamcrest-1.3.pdf
+ */
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
