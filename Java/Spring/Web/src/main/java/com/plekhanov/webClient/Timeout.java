8e11785697f5590e5c53a46c24eadeda7b6440aa
Plekhanov <atagan@rambler.ru>
2021-05-24 23:17:45 +0300

    web client

--- Java/Spring/Web/src/main/java/com/plekhanov/webClient/Timeout.java
+++ Java/Spring/Web/src/main/java/com/plekhanov/webClient/Timeout.java
@@ -6,6 +6,7 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
 
@@ -13,7 +14,10 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * https://www.baeldung.com/spring-5-webclient
+ * https://docs.spring.io/spring-framework/docs/5.3.0-SNAPSHOT/spring-framework-reference/web-reactive.html#webflux-client
  * https://vk.com/@javatutorial-shpargalka-po-spring-boot-webclient
+ *
+ * WebClient with Timeout
  */
 public class Timeout {
 
@@ -21,6 +25,20 @@ public class Timeout {
     public static final int TIMEOUT = 1000;
 
     public static void main(String[] args) {
+        WebClient webClient = getWebClient();
+
+        Mono<String> mono = webClient.get()
+                .uri("/maps")
+                .retrieve()
+                .bodyToMono(String.class);
+
+        System.out.println(mono.block());
+
+    }
+
+
+
+    public static WebClient getWebClient() {
         TcpClient tcpClient = TcpClient
                 .create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
@@ -29,7 +47,7 @@ public class Timeout {
                     connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
                 });
 
-        WebClient client2 = WebClient.builder()
+        return WebClient.builder()
                 .baseUrl(BASE_URL)
                 .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
                 .build();
