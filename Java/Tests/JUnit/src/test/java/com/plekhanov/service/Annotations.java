39ee4f65f8107b92c8d0bce0c8c7da2aabcd5cbc
Plekhanov <atagan@rambler.ru>
2021-05-26 09:40:07 +0300

    test

--- Java/Tests/JUnit/src/test/java/com/plekhanov/service/Annotations.java
+++ Java/Tests/JUnit/src/test/java/com/plekhanov/service/Annotations.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.springframework.test.annotation.DirtiesContext;
 
+
+/**
+ *  {@link DirtiesContext}
+ *
+ */
 public class Annotations {
-
     @EnabledOnOs(OS.WINDOWS)
     @EnabledIfEnvironmentVariable(named = "", matches= "")
     @DisplayName("описание теста выводимое в IDE")
